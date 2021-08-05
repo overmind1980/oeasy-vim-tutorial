53783fc4c24d1416289c69b370d3db76bbf87d9d
oeasy <easyo@easydeMBP.lan>
2021-08-05 20:30:55 +0800

    yes

---
+++ dos.java
@@ -0,0 +1,12 @@
+public class oeasy{
+    public static void o2z(){
+        System.out.println("oeasy");
+        System.out.println("o2z");
+        System.out.println("o3z");
+    }
+    public static void main(String[] args){
+        System.out.println("========before oeasy==========");
+        o2z();
+        System.out.println("========after oeasy==========");
+    }
+}

699d2531ac85266201325ab9aa5c457c921f1704
overmind1980 <easyo@easydeMBP.lan>
2021-09-13 13:27:57 +0800

    yes :

--- dos.java
+++ dos.java
@@ -2,7 +2,7 @@ public class oeasy{
     public static void o2z(){
         System.out.println("oeasy");
         System.out.println("o2z");
-        System.out.println("o3z");
+        System.out.println("o3zz");
     }
     public static void main(String[] args){
         System.out.println("========before oeasy==========");
